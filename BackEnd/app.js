1365918ce01cdb234a4ee3fec48be7fa445f78bd
Abdulla <pikasu2000@gmail.com>
2025-09-22 16:41:07 +0530

    update_vercel.json

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -13,13 +13,19 @@ const connectDB = require("./config/db");
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send({
+    activeStatus: true,
+    error: false,
+  });
+});
+
 app.use("/api/auth", require("./routes/user.route"));
 app.use("/api/pets", require("./routes/pet.route"));
 app.use("/api/appointments", require("./routes/appointment.route"));
 app.use("/api/activity", require("./routes/activity.route"));
 app.use("/api/insurance", require("./routes/insurance.route"));
 app.use("/api/admin", require("./routes/admin.route"));
-
 
 app.use((req, res) => {
   res.status(404).send("404 Not Found");
